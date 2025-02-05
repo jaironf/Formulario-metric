c51831e89fd34dad5e176f369bf7b12ae6301f63
jaironf <jaironf.94@gmail.com>
2025-02-05 20:44:47 +0100

    Email y numero tel registrados de ejemplo implementados

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('userForm');
     const infoCard = document.getElementById('infoCard');
 
+    const registeredEmails = ['user1@example.com', 'user2@example.com'];
+    const registeredPhones = ['123456789', '987654321'];
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -24,12 +26,18 @@ document.addEventListener('DOMContentLoaded', function() {
         if(!validateEmail(email.value)) {
             setError(email, 'Email is required');
             valid = false;
+        } else if(registeredEmails.includes(email.value)){
+            setError(email, 'The email is already registered');
+            valid = false
         } else {
             clearError(email)
         }
 
         if(!validatePhone(phone.value)) {
             setError(phone, 'Phone is required')
+        } else if(registeredPhones.includes(phone.value)) {
+            setError(phone, 'The number is already registered');
+            valid = false
         } else {
             clearError(phone)
         }
