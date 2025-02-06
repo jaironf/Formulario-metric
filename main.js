6c064003ec33fc8343721299e7d532baa5dbef06
jaironf <jaironf.94@gmail.com>
2025-02-06 18:10:24 +0100

    Lógica implementada

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        const name = document.getElementById('name').Value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const phone = document.getElementById('phone').value;
-        const message = document.getElementById('message').value;
+        const name = document.getElementById('name');
+        const email = document.getElementById('email');
+        const password = document.getElementById('password');
+        const phone = document.getElementById('phone');
+        const message = document.getElementById('message');
 
         let valid = true;
 
@@ -48,6 +48,57 @@ document.addEventListener('DOMContentLoaded', function() {
             clearError(message)
         }
 
+        if (valid) {
+            simulateAjaxRequest({
+                name: name.value,
+                email: email.value,
+                password: password.value,
+                phone: phone.value,
+                message: message.value,
+            });
 
+            registeredEmails.push(email.value)
+            registeredPhones.push(phone.value)
+
+        }
     })
+
+    function setError(element, message) {
+        const errorMessage = element.nextElementSibling;
+        errorMessage.textContent = message;
+        errorMessage.style.display = 'block';
+    }   
+
+    function clearError(element) {
+        const errorMessage = element.nextElementSibling;
+        errorMessage.textContent = '';
+        errorMessage.style.display = 'none';
+    }
+
+    function validateEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        return emailPattern.test(email)
+    }
+
+    function validatePhone(phone) {
+        const phonePattern = /^[0-9]{9}$/
+        return phonePattern.test(phone)
+    }
+
+    function simulateAjaxRequest(data) {
+        setTimeout(() => {
+            document.getElementById('cardName').textContent = data.name;
+            document.getElementById('cardEmail').textContent = data.email;
+            document.getElementById('cardPassword').textContent = data.password
+            document.getElementById('cardPhone').textContent = data.phone;
+            document.getElementById('cardMessage').textContent = data.message;
+
+            infoCard.classList.remove('hidden')
+            infoCard.classList.add('show')
+
+            console.log('Datos enviados correctamente', data)
+        }, 2000)  
+    }
+
 })
+
